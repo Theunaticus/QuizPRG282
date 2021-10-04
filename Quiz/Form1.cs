08d6ebe13f649f4478bc2c124b6af7ec41363f77
Theunaticus <84011334+Theunaticus@users.noreply.github.com>
2021-10-04 13:44:43 +0200

    Update Form1.cs
    Did the stuffs

--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Quiz
 {
@@ -15,6 +16,20 @@ namespace Quiz
         public Form1()
         {
             InitializeComponent();
+            ReadFile();
+        }
+        
+        public  void    ReadFile    ()  {
+            string path = @"C:\Users\Thenus\Documents\BC\PRG282\QuizPRG282\Quiz\Quiz\bin\Debug\New Text Document.txt";
+            
+            FileStream MyStream = new FileStream(path,FileMode.OpenOrCreate);
+            StreamReader reader= new StreamReader(MyStrean);
+            string  Line;
+            while ((Line = reader.ReadLine())!=null){
+                listBox1.Lines.Add(Line);
+            }
+            reader.Close();
+            MyStream.Close();
         }
     }
 }
